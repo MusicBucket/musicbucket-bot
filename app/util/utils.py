700eb6da1cf355d3b0dd75bdbae89704aeaccd24
Pau Riera <pau.riera.forteza@gmail.com>
2019-02-10 20:34:35 +0100

    Fix extracting url

--- app/util/utils.py
+++ app/util/utils.py
@@ -8,5 +8,7 @@ logger = logging.getLogger(__name__)
 def extract_url_from_message(text):
     """Gets the first url of a message"""
     logger.info(f'Extracting url from message: {text}')
-    link = re.search("(?P<url>https?://[^\s]+)", text).group("url")
-    return link
+    link = re.search("(?P<url>https?://[^\s]+)", text)
+    if link is not None:
+        return link.group('url')
+    return ''
